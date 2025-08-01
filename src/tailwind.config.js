32da6b52a788fd97a4975dbc29e131ce37f7f3d8
firstproject <jayedkhan563737@gmail.com>
2025-08-01 10:57:49 +0600

    first tailwind css project

---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   darkMode:'class',
+   content: ["./src/**/*.{html,js}","./index.html"],
+   theme: {
+     extend: {
+        colors:{
+            primary:'#DC7633',
+        },
+        
+     },
+   },
+   plugins: [],
+ }
